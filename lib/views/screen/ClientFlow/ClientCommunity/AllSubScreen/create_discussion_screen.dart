fff7c3922ed40001b46700d1763e5542a0f49900
Md Limon Islam <limon222019@gmail.com>
2025-08-14 11:08:05 +0600

    complete bottom sheet

---
+++ lib/views/screen/ClientFlow/ClientCommunity/AllSubScreen/create_discussion_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateDiscussionScreen extends StatefulWidget {
+  const CreateDiscussionScreen({super.key});
+
+  @override
+  State<CreateDiscussionScreen> createState() => _CreateDiscussionScreenState();
+}
+
+class _CreateDiscussionScreenState extends State<CreateDiscussionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/views/screen/ClientFlow/ClientCommunity/client_community_screen.dart
+++ lib/views/screen/ClientFlow/ClientCommunity/client_community_screen.dart
@@ -1,6 +1,8 @@
 import 'package:find_me_a_coach/utils/app_colors.dart';
 import 'package:find_me_a_coach/views/base/client_bottom_menu..dart';
+import 'package:find_me_a_coach/views/base/custom_text_field.dart';
 import 'package:find_me_a_coach/views/screen/ClientFlow/AiChatBoot/ai_chat_screen.dart';
+import 'package:find_me_a_coach/views/screen/ClientFlow/ClientCommunity/AllSubScreen/create_discussion_screen.dart';
 import 'package:find_me_a_coach/views/screen/ClientFlow/Notification/notification_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -66,94 +68,109 @@ class ClientCommunityScreen extends StatelessWidget {
                 child: ListView.separated(
                   physics: AlwaysScrollableScrollPhysics(),
                 itemBuilder: (context, index){
-                  return Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                        color: AppColors.textColor,
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0xFF000000).withValues(alpha: 0.05),
-                            blurRadius: 10,
-                            offset: Offset(0, 3),
-                          ),
-                        ]
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              height: 32,
-                              width: 32,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(image: AssetImage('assets/images/person.jpg'),
-                                      fit: BoxFit.cover)
-                              ),
+                  return InkWell(
+                    onTap: (){
+
+                      showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true, // so it adjusts for the keyboard
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                        ),
+                        builder: (context) {
+                          return CommentBottomSheet();
+                        },
+                      );
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                          color: AppColors.textColor,
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFF000000).withValues(alpha: 0.05),
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
                             ),
-                            SizedBox(width: 8,),
-                         Column(
-                           mainAxisAlignment: MainAxisAlignment.start,
-                           crossAxisAlignment: CrossAxisAlignment.start,
-                           children: [
-                             Text("Alex Johnson",
+                          ]
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                height: 32,
+                                width: 32,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(image: AssetImage('assets/images/person.jpg'),
+                                        fit: BoxFit.cover)
+                                ),
+                              ),
+                              SizedBox(width: 8,),
+                           Column(
+                             mainAxisAlignment: MainAxisAlignment.start,
+                             crossAxisAlignment: CrossAxisAlignment.start,
+                             children: [
+                               Text("Alex Johnson",
+                                 style: TextStyle(
+                                   color: Color(0xFF2D2D2D),
+                                   fontSize: 16,
+                                   fontWeight: FontWeight.w400,
+                                 ),),
+                               SizedBox(height: 4,),
+                               Text("10 min",
                                style: TextStyle(
-                                 color: Color(0xFF2D2D2D),
-                                 fontSize: 16,
-                                 fontWeight: FontWeight.w400,
-                               ),),
-                             SizedBox(height: 4,),
-                             Text("10 min",
-                             style: TextStyle(
-                               color: Color(0xFFAFAFAF),
-                               fontSize: 10,
-                               fontWeight: FontWeight.w400
-                             ),)
-                           ],
-                         )
+                                 color: Color(0xFFAFAFAF),
+                                 fontSize: 10,
+                                 fontWeight: FontWeight.w400
+                               ),)
+                             ],
+                           )
 
 
-                          ],
-                        ),
-                        SizedBox(height: 10,),
-                        Divider(
-                          color: Color(0xFFE6ECF3),
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Topic: Confidence",
-                        style: TextStyle(
-                          color: Color(0xFF9CA3AF),
-                          fontSize: 10,
-                          fontWeight: FontWeight.w600,
-                        ),),
-                        SizedBox(height: 4,),
-                        Text("How to build confidence in the workplace?",
-                        style: TextStyle(
-                          color: Color(0xFF222222),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                        ),),
-                        SizedBox(height: 20,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset('assets/icons/secondaryComment.svg'),
-                            SizedBox(width: 4,),
-                            Text("Comment (20)",
-                              style: TextStyle(
-                                color: Color(0xFF4B4B4B),
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                              ),)
+                            ],
+                          ),
+                          SizedBox(height: 10,),
+                          Divider(
+                            color: Color(0xFFE6ECF3),
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Topic: Confidence",
+                          style: TextStyle(
+                            color: Color(0xFF9CA3AF),
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600,
+                          ),),
+                          SizedBox(height: 4,),
+                          Text("How to build confidence in the workplace?",
+                          style: TextStyle(
+                            color: Color(0xFF222222),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),),
+                          SizedBox(height: 20,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset('assets/icons/secondaryComment.svg'),
+                              SizedBox(width: 4,),
+                              Text("Comment (20)",
+                                style: TextStyle(
+                                  color: Color(0xFF4B4B4B),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                ),)
 
-                          ],
-                        )
-                      ],
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   );
                 },
@@ -169,7 +186,9 @@ class ClientCommunityScreen extends StatelessWidget {
           borderRadius: BorderRadius.circular(100),
 
         ),
-        onPressed: (){},
+        onPressed: (){
+          Get.to(()=> CreateDiscussionScreen());
+        },
         child: SvgPicture.asset('assets/icons/add.svg'),
       ),
       bottomNavigationBar: ClientBottomMenu(2),
@@ -221,3 +240,134 @@ class ClientCommunityScreen extends StatelessWidget {
     );
   }
 }
+
+class CommentBottomSheet extends StatefulWidget {
+  const CommentBottomSheet({super.key});
+
+  @override
+  State<CommentBottomSheet> createState() => _CommentBottomSheetState();
+}
+
+class _CommentBottomSheetState extends State<CommentBottomSheet> {
+
+  final commentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.8,
+      minChildSize: 0.5,
+      maxChildSize: 0.95,
+      expand: false,
+      builder: (context, scrollController) {
+        return Container(
+
+          decoration: BoxDecoration(
+            color: AppColors.backgroundColor,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: Column(
+            children: [
+
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Comments",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF1F2937),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+              // Comment list
+              Expanded(
+                child: ListView.separated(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+                  controller: scrollController,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: 18,
+                          backgroundImage:
+                          AssetImage('assets/images/person.jpg'),
+                        ),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Henry, Arthur",
+                                style: TextStyle(fontWeight: FontWeight.w600,
+                                color: Color(0xFF1F2937),
+                                fontSize: 14),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                "Lorem ipsum dolor sit amet consectetur.",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xFF4B5563)
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                  separatorBuilder: (_, __) => SizedBox(height: 12),
+                  itemCount: 10,
+                ),
+              ),
+
+              // Input field
+              SafeArea(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: AppColors.textColor
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CustomTextField(controller: commentController,
+                        hintText: 'Write a comment....',),
+                      ),
+                      SizedBox(width: 8),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFE6ECF3),
+                        border: Border.all(color: Color(0xFFB0C4DB),width: 2)
+                      ),
+                      child: Center(
+                        child: SvgPicture.asset('assets/icons/send.svg'),
+                      ),
+                    )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
