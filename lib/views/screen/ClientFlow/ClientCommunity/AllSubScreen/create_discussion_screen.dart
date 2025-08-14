cd97f0b4a59655a3b5c88711f8cc67e11eb446f9
Md Limon Islam <limon222019@gmail.com>
2025-08-14 11:20:44 +0600

    complete create discussion screen

--- lib/views/screen/ClientFlow/ClientCommunity/AllSubScreen/create_discussion_screen.dart
+++ lib/views/screen/ClientFlow/ClientCommunity/AllSubScreen/create_discussion_screen.dart
@@ -1,3 +1,7 @@
+import 'package:find_me_a_coach/utils/app_colors.dart';
+import 'package:find_me_a_coach/views/base/custom_appbar.dart';
+import 'package:find_me_a_coach/views/base/custom_button.dart';
+import 'package:find_me_a_coach/views/base/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class CreateDiscussionScreen extends StatefulWidget {
@@ -8,8 +12,62 @@ class CreateDiscussionScreen extends StatefulWidget {
 }
 
 class _CreateDiscussionScreenState extends State<CreateDiscussionScreen> {
+
+  final topicTextController = TextEditingController();
+  final askTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: CustomAppbar(title: "Create Discussion"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Topic",
+                    style: TextStyle(
+                     fontSize: 18,
+                     fontWeight: FontWeight.w600,
+                     color: Color(0xFF4B5563)
+                    ),),
+                    SizedBox(height: 8,),
+                    CustomTextField(controller: topicTextController,
+                    suffixIcon: Icon(Icons.keyboard_arrow_down,
+                    color: AppColors.bigTextColor,),
+                    hintText: 'Select Topic',),
+                    SizedBox(height: 16,),
+                    Text("Write your post",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF4B5563)
+                      ),),
+                    SizedBox(height: 8,),
+                    CustomTextField(controller: topicTextController,
+                      maxLines: 5,
+                      minLines: null,
+                      hintText: 'Ask a question....',),
+
+
+
+                  ],
+
+                ),
+              ),
+            ),
+
+            CustomButton(onTap: (){},
+                text: "Post")
+          ],
+        ),
+      ),
+    );
   }
 }
